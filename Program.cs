471f54df62b36a713f3f8b8bb3e86c17ba3c759d
Jarvis <jarvis.avinash@gmail.com>
2024-11-23 01:32:52 +0530

    Added Login with Google

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configure authentication services
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddGoogle(options =>
+{
+    options.ClientId = "1051126877348-p94s8drl9bbdmc6uv1m2v1dkkvr25ugp.apps.googleusercontent.com";
+    options.ClientSecret = "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";
+    options.CallbackPath = "/signin-google"; // Match the redirect URI
+});
 
 var app = builder.Build();
 
@@ -18,6 +35,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// Ensure middleware order: Authentication must come before Authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
@@ -25,4 +44,3 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
